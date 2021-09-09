ece189e57cf7d618aa51ab8c24dce2174a3f668d
Valeria Kapustina <lerka141299@gmail.com>
2021-09-09 13:05:57 +0700

    Simple Bot versia_1

---
+++ bot.py
@@ -0,0 +1,36 @@
+import logging
+import settings
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+
+# Displays the time, the time level, and a message about what happened to the telegram channel.
+logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(message)s',
+					level = logging.INFO,
+					filename = 'bot.log'
+					)
+
+def start_bot(update: Updater, context: CallbackContext):
+	print(update)
+	mytext = """Hello {}
+
+	I have only /start command! =)""".format(update.message.chat.first_name)
+	update.message.reply_text(mytext)
+
+def chat(update: Updater, context: CallbackContext):
+	text = update.message.text
+	logging.info(text)
+#	text = "Hello"
+
+	update.message.reply_text(text)
+
+def main():
+	updtr = Updater(settings.TOKEN_TELEGRAMM)
+
+	updtr.dispatcher.add_handler(CommandHandler("start", start_bot))
+	updtr.dispatcher.add_handler(MessageHandler(Filters.text, chat))
+# Connecting to the platform
+	updtr.start_polling()
+	updtr.idle()
+
+if __name__ == "__main__":
+	logging.info('Bot started!')
+	main()
